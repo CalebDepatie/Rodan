84ce9fc5f2be92e002ed774e4f43c2f42bfc3b4b
Caleb Depatie <caleb.depatie@outlook.com>
2021-08-15 11:55:55 -0400

    Added audio playback
    Mostly for future pings/notifications, but right now for a fun radio wave monster 'easter egg'

--- ui/src/Modules/home.tsx
+++ ui/src/Modules/home.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
+import { Sound } from "../Components";
 
 function Home() {
+  let radioWaveMonster = useRef<any>(null);
+
 	return (
 	<div className="home">
       <div className="container">
@@ -12,7 +16,11 @@ function Home() {
             </p>
           </div>
           <div className="col-lg-7">
-            <img src="icon.png" className="img-fluid rounded mb-4 mb-lg-0"/>
+            <Sound ref={radioWaveMonster} soundFile={"RODAN RODAN RADIO WAVE MONSTER.mp3"} />
+            <img src="icon.png" className="img-fluid rounded mb-4 mb-lg-0"
+              onClick={() => {
+                radioWaveMonster.current!.play()
+              }}/>
           </div>
         </div>
       </div>
