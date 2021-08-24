d3e03a72c21cf7b3bc4441a3b4d38b7cc478cb42
Caleb Depatie <caleb.depatie@outlook.com>
2021-08-24 19:16:14 -0400

    Basic Tasks Infrastructure

---
+++ lib/funcs/tasks.go
@@ -0,0 +1,109 @@
+package funcs
+
+import (
+	"github.com/guark/guark/app"
+  "github.com/CalebDepatie/ProjectSingularPoint/lib/globals"
+  "encoding/json"
+  "errors"
+  "strings"
+)
+
+func GetTasks(c app.Context) (interface{}, error) {
+  comps := []struct {
+    Id       string `db:"id" json:"id"`
+    Title    string `db:"title" json:"title"`
+    Descrip  string `db:"description" json:"descrip"`
+    Activity string `db:"activity" json:"activity"`
+    Status   int    `db:"status" json:"status"`
+    Created  string `db:"created_date" json:"created"`
+  }{}
+
+  stmt := `SELECT * FROM prj.FN_TaskCRUD(_operation := 2);`
+  err  := globals.DB.Select(&comps, stmt)
+  if err != nil {
+    c.App.Log.Error("Error getting data: ", err.Error())
+    return "{}", err
+  }
+
+  bytes, err := json.Marshal(comps)
+
+  if err != nil {
+    c.App.Log.Error("Error: ", err.Error())
+    return "{}", err
+  }
+
+  return string(bytes), nil
+}
+
+func CreateTask(c app.Context) (interface{}, error) {
+  var (
+    stmt string
+    err  error
+    unmarshalErr *json.UnmarshalTypeError
+  )
+
+  p := struct{
+    Title    string `db:"title" json:"title"`
+    Descrip  string `db:"description" json:"description"`
+    Status   int    `db:"status" json:"status"`
+    Activity string `db:"activity" json:"activity"`
+  }
+
+  dec := json.NewDecoder(strings.NewReader(c.GetOr("body", "").(string)))
+  err = dec.Decode(&p)
+
+  if err != nil {
+      if errors.As(err, &unmarshalErr) {
+        c.App.Log.Error("JSON Error: ", unmarshalErr.Field)
+      } else {
+        c.App.Log.Error("Request Error: ", err.Error())
+      }
+      return "{}", err
+  }
+
+  stmt   = `SELECT * FROM prj.FN_TaskCRUD(_operation := 1, _title := $1, _description := $2, _status := $3, _activity := $4);`
+  _, err = globals.DB.Exec(stmt, p.Title, p.Descrip, p.Status, p.Activity)
+
+  if err != nil {
+    c.App.Log.Error("Error getting data: ", err.Error())
+    return "{}", err
+  }
+
+  return "{}", nil
+}
+
+func UpdateTask(c app.Context) (interface{}, error) {
+  var (
+    stmt string
+    err  error
+    unmarshalErr *json.UnmarshalTypeError
+  )
+
+  p := struct {
+    TaskID    string `json:"taskID"`
+    UpdateVal string `json:"updateVal"`
+    UpdateCol string `json:"updateCol"`
+  }{}
+
+  dec := json.NewDecoder(strings.NewReader(c.GetOr("body", "").(string)))
+  err = dec.Decode(&p)
+
+  if err != nil {
+      if errors.As(err, &unmarshalErr) {
+        c.App.Log.Error("JSON Error: ", unmarshalErr.Field)
+      } else {
+        c.App.Log.Error("Request Error: ", err.Error())
+      }
+      return "{}", err
+  }
+
+  stmt = `SELECT * FROM prj.FN_TaskCRUD(_operation := 3, _TaskID := $1, _updateVal := $2, _updateCol := $3);`
+  _, err = globals.DB.Exec(stmt, p.TaskID, p.UpdateVal, p.UpdateCol)
+
+  if err != nil {
+    c.App.Log.Error("Error getting data: ", err.Error())
+    return "{}", err
+  }
+
+  return "{}", nil
+}
---
+++ scripts/prj/FN_TaskCRUD.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE FUNCTION prj.FN_TaskCRUD (
+    _operation INT,
+
+    _title       VARCHAR(255) = NULL,
+    _description VARCHAR      = NULL,
+    _activity    VARCHAR(36)  = NULL,
+
+    _taskID    VARCHAR(36)  = NULL,
+    _updateVal VARCHAR(256) = NULL,
+    _updateCol VARCHAR(256) = NULL
+) RETURNS TABLE (id varchar(36), title varchar(255), description varchar, activity varchar(36), created_date float)
+AS $$
+BEGIN
+  IF _operation = 1 THEN
+    INSERT INTO prj.tasks (title, description, activity, created_date)
+      VALUES (_title, _description, _activity, NOW()::DATE);
+
+  ELSIF _operation = 2 THEN
+    RETURN QUERY
+    SELECT TSK.id, TSK.title, COALESCE(TSK.description, ''), TSK.status, COALESCE(TSK.activity, ''), EXTRACT(EPOCH FROM TSK.created_date)
+      FROM prj.tasks AS TSK;
+
+  ELSIF _operation = 3 THEN
+    EXECUTE FORMAT('UPDATE prj.tasks SET %I = $1::%s WHERE prj.tasks.id = $2', _updateCol, (SELECT data_type FROM information_schema.columns WHERE table_name = 'tasks' AND column_name = _updateCol))
+      USING _updateVal, _taskID;
+
+  ELSIF _operation = 4 THEN
+    DELETE FROM prj.tasks WHERE prj.tasks.id = _taskID;
+
+  END IF;
+END; $$
+LANGUAGE plpgsql;
