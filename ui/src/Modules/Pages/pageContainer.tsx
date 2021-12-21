793387de0420d82ef47e83a7d87a56f28f4c7241
Caleb Depatie <caleb.depatie@outlook.com>
2021-12-21 18:36:22 -0500

    Worked on JJ Module

--- ui/src/Modules/Pages/pageContainer.tsx
+++ ui/src/Modules/Pages/pageContainer.tsx
@@ -3,14 +3,19 @@ import React from 'react';
 import PageViewer from './pageViewer';
 
 import { Tree } from 'primereact/tree';
+import { Button } from '../../Components';
 
 function PageContainer(props:{}) {
 
   return (
     <div style={{display:"flex"}}>
-      <Tree style={{width:"300px", marginRight:"5px"}}>
+      <div style={{width:"300px", marginRight:"5px"}}>
+        <Button label="Add Page"/>
+        <Tree>
 
-      </Tree>
+        </Tree>
+      </div>
+
       <PageViewer page={"test *bold test*"} />
     </div>
   );
