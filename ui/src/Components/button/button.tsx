46a4ac62d169331806223ad0c6f4a8f4d0053e1d
Caleb Depatie <caleb.depatie@outlook.com>
2021-12-21 20:45:59 -0500

    Moved to using 'disabled' attribute for CSS

--- ui/src/Components/button/button.tsx
+++ ui/src/Components/button/button.tsx
@@ -5,13 +5,11 @@ import './button.scss';
 function Button(props:{label?:string, icon?:string, className?:string, disabled?:boolean,
     onClick?:(e:any)=>void, style?:{[key:string]: string}, children?:React.ReactNode}) {
 
-  const className:string = props.className + (props.disabled ? ' disabled' : ' ');
-
   const label = props.label ? <span>{props.label}</span> : null;
   const icon  = props.icon ? <span className={props.icon} /> : null;
 
   return (
-    <button className={className} disabled={props.disabled} onClick={props.onClick} style={props.style}>
+    <button className={props.className} disabled={props.disabled} onClick={props.onClick} style={props.style}>
       {icon}
       {label}
       {props.children}
--- ui/src/Components/inputtext/inputtext.tsx
+++ ui/src/Components/inputtext/inputtext.tsx
@@ -5,11 +5,9 @@ import "./inputtext.scss";
 function InputText(props:{value:any, onChange?:(e:any)=>void, type?:string, id?:string,
     className?:string, disabled?:boolean, style?:{[key:string]: string}}) {
 
-  const className = props.className + (props.disabled ? ' disabled' : '');
-
   return (
-    <input value={props.value} type={props.type}  onChange={props.onChange} id={props.id}
-      className={className} disabled={props.disabled} style={props.style} />
+    <input value={props.value} type={props.type} onChange={props.onChange} id={props.id}
+      className={props.className} disabled={props.disabled} style={props.style} />
   );
 };
 
