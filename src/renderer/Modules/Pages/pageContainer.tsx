4195f87adb39ee714ebb3a6b387a379fd5d6ee94
Caleb Depatie <caleb.depatie@outlook.com>
2022-05-10 20:40:32 -0400

    More Consistent Styling

--- src/renderer/Modules/Pages/pageContainer.tsx
+++ src/renderer/Modules/Pages/pageContainer.tsx
@@ -78,7 +78,7 @@ function PageContainer(props:{}) {
         <Button label="Add Page" onClick={() => setShowForm(true)}/>
         <Button label={edit ? "Publish Page" : "Edit Page"} onClick={() => setEdit(!edit)} />
         <Tree value={nodes} selectionMode="single" selectionKeys={selectedKey}
-          style={{height:"calc(100vh - 126px)", overflowY:'scroll'}}
+          style={{height:"calc(100vh - 110px)", overflowY:'scroll'}}
           onSelectionChange={(e:any) => setSelectedKey(e.value)} />
       </div>
 
