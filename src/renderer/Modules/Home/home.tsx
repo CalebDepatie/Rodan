30f581d571fe260b16282a72c891bc6cfba39736
Caleb Depatie <caleb.depatie@outlook.com>
2023-03-10 13:56:11 -0500

    The app is primarily not accessible prior to logging in

--- src/renderer/Modules/Home/home.tsx
+++ src/renderer/Modules/Home/home.tsx
@@ -3,17 +3,23 @@ import path from 'path'
 import { Card } from "../../Components"
 import { Login } from "./login"
 import { FinanceCard } from "../Finances"
-import { useSound } from "../../Hooks"
+import { useSound, useMemoryStorage } from "../../Hooks"
 
 import "./home.scss"
 
 export const Home = () => {
+  const { value: loggedIn } = useMemoryStorage("login", false)
   const radioWaveMonster = useSound("assets/RODAN RODAN RADIO WAVE MONSTER.mp3")
 
 	return (
 	<div className="r-home">
     <Login />
-    <FinanceCard />
+
+    {
+      (loggedIn) ?
+        <FinanceCard />
+      : null
+    }
 
     <Card>
       <img src="assets/icon.png" style={{width:"230px", height:"230px"}}
