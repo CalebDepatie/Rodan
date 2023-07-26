301d477eaebf05b1912560531c1e96736879a05e
Caleb Depatie <caleb.depatie@outlook.com>
2023-07-26 16:33:32 -0400

    Swapped to one consistent way of declaring new pages

--- src/renderer/Components/header/header.tsx
+++ src/renderer/Components/header/header.tsx
@@ -5,25 +5,46 @@ import { useMemoryStorage } from '../../Hooks'
 
 import "./header.scss";
 
-function Header(props: {}) {
+function Header(props: {modules: any[]}) {
   const { value: loggedIn } = useMemoryStorage("login", false)
+
+  const generateHeaderRoutes = (modules: any[], prefix?: string) => {
+    let headerRoutes:any[] = []
+    prefix ??= ""
+
+    modules.forEach((module) => {
+      if (module.display !== false && !module.modules) {
+        headerRoutes.push(<>
+          <Link key={prefix + module.path} to={prefix + module.path}>{module.name}</Link>
+        </>)
+      }
+
+      if (module.modules) {
+        const subheader = generateHeaderRoutes(module.modules, prefix + module.path)
+        
+        if (module.display !== false) {
+          var header = <Link to={prefix + module.path}>{module.name}</Link>
+        }
+
+        headerRoutes.push(
+          <HeaderNest key={prefix + module.path} header={header}>
+            {subheader}
+          </HeaderNest>
+        )
+      }
+    })
+
+    return headerRoutes
+  }
 
 	return (
     <div className="r-header">
       <Link to="/" id="title" >Project Singular Point</Link>
 
       <div className="header-right">
-      { // todo: generate dynamically
+      {
         (loggedIn) ?
-        <>
-          <HeaderNest header={<Link to="/projects/raw">Projects</Link>}>
-            <Link to="/projects/boards">Boards</Link>
-            <Link to="/projects/tasks">Tasks</Link>
-          </HeaderNest>
-
-          <Link to="/finances/dashboard">Finances</Link>
-          <Link to="/documentation">Pages</Link>
-        </>
+          generateHeaderRoutes(props.modules)
         : null
       }
       </div>
