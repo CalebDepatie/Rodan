b57fb25f90491ac365278bfc88792d76b440c0f9
Caleb Depatie <caleb.depatie@outlook.com>
2023-10-02 16:52:15 -0400

    Climate Clock uses cache for snappy loading

--- src/renderer/Modules/Home/climate_clock.tsx
+++ src/renderer/Modules/Home/climate_clock.tsx
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { ipcRenderer } from 'electron'
 import { Card } from '../../Components'
+import { useCache } from '../../Hooks';
 import dayjs from 'dayjs';
 
 // Using https://github.com/climateclock/climate-clock-widget as a general base
 export function ClimateClock(props:{}) {
-  const [cdata, setCdata] = useState<any>(null);
-
-  useEffect(() => {
-    const fn = async () => {
-      const res = await ipcRenderer.invoke("climate-clock-get", null)
-      setCdata(res)
-    }
-    fn()
-  }, [])
+  const [climate_cache, climate_signal] = useCache('climate-clock-get', {})
 
   const remaining = () => {
-    if (!cdata) return ""
+    if (Object.keys(climate_cache).length === 0) return ""
 
-    const date = dayjs(cdata.deadline.timestamp)
+    const date = dayjs(climate_cache.deadline.timestamp)
     const now = dayjs()
     const years = date.diff(now, "year")
     const days = date.diff(now, "day") - (years * 365)
@@ -27,11 +20,11 @@ export function ClimateClock(props:{}) {
   }
 
   const renewables = () => {
-    if (!cdata) return ""
+    if (Object.keys(climate_cache).length === 0) return ""
 
     const now = dayjs()
-    const tElapsed = now.diff(dayjs(cdata.renewables.timestamp))
-    const percent = cdata.renewables.initial + ((tElapsed / 1000) * cdata.renewables.rate)
+    const tElapsed = now.diff(dayjs(climate_cache.renewables.timestamp))
+    const percent = climate_cache.renewables.initial + ((tElapsed / 1000) * climate_cache.renewables.rate)
     
     return percent.toFixed(5)
   }
