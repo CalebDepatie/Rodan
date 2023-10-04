bd4e8bd9c5d32167e4273f567f15e8ded416c5cb
Caleb Depatie <caleb.depatie@outlook.com>
2023-10-04 17:42:26 -0400

    Evenly spaced row cells

--- src/renderer/Components/treetable/treetable.tsx
+++ src/renderer/Components/treetable/treetable.tsx
@@ -11,26 +11,33 @@ import { relative } from 'path';
 
 function ExpandedableRow(props:{value:TreeNode, columns:Column[], 
 	selectionKey?:Identifier, onSelectionChange?:(e:any) => void, width:string, level?:number}) {
-	const [expanded, setExpanded] = useState(false);
+	
+		const [expanded, setExpanded] = useState(false);
 	const buttonIcon = expanded ? "fa fa-angle-down" : "fa fa-angle-right"
 
 	return (<div className={"r-treetable-group" + (expanded ? " expanded" : "")}>
 		<Row className={props.selectionKey === props.value.key ? "r-treetable-selected" : ""} key={props.value.key} 
 			onClick={(evt) => props.onSelectionChange?.({...evt, value: props.value.key})}>
+			
 			{props.columns.map((col:Column, idx:number) => {
 				let body = (col.body != undefined) ? <col.body {...props.value} />
 											: props.value.data?.[col.field]
 
+				const isExpander = idx === 0 && props.value.children?.length > 0;
+
+				const padding = (props.level??0)*30 + "px"
+				const width = isExpander ? `calc(${props.width} - 24px)` : props.width
+
 				let ret = [
 					<Cell key={props.value.key + "-" + col.field}
-						style={{width: props.width, paddingLeft:(props.level??0)*30 + "px", ...col.style}}
+						style={{width: width, paddingLeft:padding, ...col.style}}
 						editor={(col.editor != undefined) ? () => <col.editor {...props.value} kkey={props.value.key} /> : undefined}>
 
 						{body}
 					</Cell>
 				]
 				
-				if (idx === 0 && props.value.children?.length > 0) {
+				if (isExpander) {
 					ret = [
 						<Button icon={buttonIcon} onClick={() => setExpanded(curState => !curState)}
 							style={{background:"transparent", color:"black"}}/>,
